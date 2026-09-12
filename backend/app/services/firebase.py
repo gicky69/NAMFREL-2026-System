96ff949e5b0381af524a6c8db820b2b497ec7b99
Joaquin Pacete <jrpacete@student.apc.edu.ph>
2026-09-12 23:59:52 +0800

    Update Dashboard UI

--- backend/app/services/firebase.py
+++ backend/app/services/firebase.py
@@ -1,26 +1,16 @@
-import os
-import json
 import firebase_admin
 from firebase_admin import auth, credentials
-
-
+ 
+ 
 def initialize_firebase():
     if not firebase_admin._apps:
-
-        firebase_credentials = os.getenv("FIREBASE_SERVICE_ACCOUNT")
-
-        if not firebase_credentials:
-            raise RuntimeError(
-                "FIREBASE_SERVICE_ACCOUNT environment variable is not set"
-            )
-
-        firebase_config = json.loads(firebase_credentials)
-
-        cred = credentials.Certificate(firebase_config)
-
+        cred = credentials.Certificate(
+            "/code/firebase-service-account.json"
+        )
+ 
         firebase_admin.initialize_app(cred)
-
-
+ 
+ 
 def verify_firebase_token(token: str):
     initialize_firebase()
     return auth.verify_id_token(token)
